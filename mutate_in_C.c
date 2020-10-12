cb23153e4193bcafadb084a4a29148c18b8c962b
keithreid-sfw <56557748+keithreid-sfw@users.noreply.github.com>
2020-10-12 17:29:20 +0100

    Update mutate_in_C.c

--- mutate_in_C.c
+++ mutate_in_C.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define ROW 2
-#define COL 3
+#include<time.h>
 
 float get_small_float()
 {
@@ -12,114 +11,30 @@ float small_float;
     return small_float;
 }
 
-int main()
-{
-    float mat1[ROW][COL], mat2[ROW][COL], mat3[ROW][COL];
-    int i, j;
-
-    printf("Built a random matrix to mock up a population");
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            mat1[i][j] = get_small_float();
-        }
-    }
-
-    printf("\n");
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            printf("%5f ", mat1[i][j]);
-        }
-        printf("\n");
-    }
-
-
-    printf("\nEnter a second matrix and throw a mutation in manually every so often: \n\n");
-
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            printf("Enter a[%d][%d]: ", i, j);
-            scanf("%f", &mat2[i][j]);
-        }
-    }
-
- 
-    {
-    printf("\n");
-    }
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            printf("%5f ", mat2[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    // add mat1 and mat2
-
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            mat3[i][j] = mat1[i][j] + mat2[i][j] ;
-        }
-    }
-
-    printf("\nResultant array: \n\n");
-
-    // print resultant array
-
-    for(i = 0; i < ROW; i++)
-    {
-        for(j = 0; j < COL; j++)
-        {
-            printf("%5f ", mat3[i][j]);
-        }
-        printf("\n");
-    }
-
-    // signal to operating system program ran fine
-    return 0;
-}
-
-
-
-=======
-    
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-/* generate adn return random ints*/
+/* generate and return random ints*/
 int * get_rand_ints()
 {
-    static int arr[10];
+    static int arr[8];
     int i;
 
     /* set the seed */
     srand ((unsigned)time(NULL));
-    for (i=0; i<10; ++i)
+    for (i=0; i<8; ++i)
     {
         arr[i] = rand();
     }
     return arr;
 }
 
-/* generate adn return random floats*/
+/* generate and return random floats*/
 float * get_rand_floats()
 {
-    static float arr[10];
+    static float arr[8];
     int i;
 
     /* set the seed */
     srand ((unsigned)time(NULL));
-    for (i=0; i<10; ++i)
+    for (i=0; i<8; ++i)
     {
         arr[i] = 100000000.0/rand();
     }
@@ -130,12 +45,12 @@ float * get_rand_floats()
 /* generate and return zeroes except one*/
 int * get_zeroes_except_one()
 {
-    static int arr[10];
+    static int arr[8];
     int i;
 
     /* set the seed */
     /*srand ((unsigned)time(NULL));*/
-    for (i=0; i<10; ++i)
+    for (i=0; i<8; ++i)
     {
         if (i == 3) {
             arr[i] = 1;
@@ -148,34 +63,91 @@ int * get_zeroes_except_one()
     return arr;
 }
 
-
-/* main to call above */
 int main()
 
 {
-     /* arrays that might be useful */
-     int  *pointer_arr_ints;
-     float *pointer_arr_floats;
-     int  *pointer_arr_zeros_except_one;
+    float mat1[8][4], mat2[8][4], mat3[8][4];
 
+    int i, j;
 
-     int   i;
+    /* arrays that might be useful */
+    int   *pointer_arr_ints;
+    float *pointer_arr_floats;
+    int   *pointer_arr_zeros_except_one;
 
-     pointer_arr_ints             = get_rand_ints();
-     pointer_arr_floats             = get_rand_floats();
-     pointer_arr_zeros_except_one = get_zeroes_except_one();
+    pointer_arr_ints             = get_rand_ints();
+    pointer_arr_floats           = get_rand_floats();
+    pointer_arr_zeros_except_one = get_zeroes_except_one();
 
-     for (i=0; i<10; i++)
-     {
+    for (i=0; i<10; i++)
+    {
          printf("\n%5d", *(pointer_arr_ints + i));
-
          printf("\n%5f", *(pointer_arr_floats + i));
-
          printf("\n%5d", *(pointer_arr_zeros_except_one + i));
-     }
+    }
 
-     return 0;
+    printf("Built a random matrix to mock up a population");
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            mat1[i][j] = get_small_float();
+        }
+    }
+
+    printf("\n");
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            printf("%5f ", mat1[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nEnter a second matrix and throw a mutation in manually every so often: \n\n");
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            printf("Enter a[%d][%d]: ", i, j);
+            scanf("%f", &mat2[i][j]);
+        }
+    }
+
+    printf("\n");
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            printf("%5f ", mat2[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    printf("\n");
+    // add mat1 and mat2
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    printf("\nResultant array: \n\n");
+    // print resultant array
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            printf("%5f ", mat3[i][j]);
+        }
+        printf("\n");
+    }
+
+    // signal to operating system program ran fine
+    return 0;
 }
-    
-    
-    
+
